69a90e0f0a151d946f495b77488764363b0633ea
Paul Jankowski <PJankowski25@gmail.com>
2015-05-12 16:14:48 -0400

    Blah

--- server/config/environment/production.js
+++ server/config/environment/production.js
@@ -19,6 +19,6 @@ module.exports = {
             process.env.MONGOHQ_URL ||
             process.env.OPENSHIFT_MONGODB_DB_URL+process.env.OPENSHIFT_APP_NAME ||
             'mongodb://localhost/bucketlist'*/
-            'mongodb://PJankowski25:Payton15@ds031581.mongolab.com:31581/heroku_app36792052'
+            'mongodb://PJankowski25:Payton15@ds031581.mongolab.com:31581/heroku_app36792621'
   }
 };
